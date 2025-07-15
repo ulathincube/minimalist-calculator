db80d48d077863a43d8b592f5775a4b555fa6b99
Ulathi Ncube <swiftly.graphics@gmail.com>
2025-07-15 15:36:20 +0200

    Feat: Add Calculator functionality on button click

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let operandA, operandB, operator;
+const calculator = document.querySelector('.calculator');
+const operatorButtons = document.querySelectorAll('.operate');
+const display = document.querySelector('.display');
+const calculateButton = document.querySelector('.calculate');
+
+const calculationsDisplay = display.querySelector('.calculations');
+const resultsDisplay = display.querySelector('.results');
+
+const operatorButtonsArr = Array.from(operatorButtons);
+
+const clearButton = document.querySelector('.clear');
+
+let operandA, operandB, operator, buttonValue, result;
+let operands = [];
 
 function add(operandA, operandB) {
   return operandA + operandB;
@@ -19,22 +32,82 @@ function divide(operandA, operandB) {
 function operate(operator, operandA, operandB) {
   switch (operator) {
     case 'add':
-      add(operandA, operandB);
+      result = add(operandA, operandB);
       break;
 
     case 'subtract':
-      subtract(operandA, operandB);
+      result = subtract(operandA, operandB);
       break;
 
     case 'multiply':
-      multiply(operandA, operandB);
+      result = multiply(operandA, operandB);
       break;
 
     case 'divide':
-      divide(operandA, operandB);
+      result = divide(operandA, operandB);
       break;
 
     default:
       return 'What is this operation?';
   }
+
+  return result;
 }
+
+calculator.addEventListener('click', event => {
+  buttonValue = event.target.dataset.value;
+
+  if (!buttonValue) return;
+
+  populateDisplay(buttonValue);
+
+  if (operatorButtonsArr.includes(event.target)) {
+    operator = event.target.dataset.operate;
+  }
+});
+
+function populateDisplay(digit) {
+  calculationsDisplay.textContent += digit;
+}
+
+calculateButton.addEventListener('click', event => {
+  const fullString = calculationsDisplay.textContent;
+
+  console.log(fullString);
+
+  switch (operator) {
+    case 'add':
+      operands = fullString.split('+');
+      break;
+
+    case 'subtract':
+      operands = fullString.split('-');
+      break;
+
+    case 'multiply':
+      operands = fullString.split('x');
+      break;
+
+    case 'divide':
+      operands = fullString.split('/');
+      break;
+  }
+
+  if (operands.length !== 2) {
+    console.log(operands[2]);
+    result = operate(operator, operands[0], operands[1]);
+    operands.splice(0, 2);
+    console.log(operands);
+
+    operands.unshift(result);
+    console.log(operands, operator);
+    result = operate(operator, operands[0], operands[1]);
+  } else {
+    [operandA, operandB] = operands.map(item => Number(item));
+    result = operate(operator, operandA, operandB);
+  }
+
+  resultsDisplay.textContent = result;
+
+  operands = [result];
+});
